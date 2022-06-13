46b586ebb6d055f70901ed0b5b0fc62330dd66ce
NaikoD <66165750+Naikod@users.noreply.github.com>
2022-06-13 12:14:51 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,2 @@
 @echo off
-git clone https://github.com/dkmpostor/node-stumble-guys && cd node-stumble-guys && npm i
-node index.js
+git clone https://github.com/dkmpostor/node-stumble-guys && cd node-stumble-guys && npm i && node index.js
